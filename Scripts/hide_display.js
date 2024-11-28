d291cf33a03b55a26a6e6a0cc8da6aee703631aa
Jobaida20 <zubayda.gulshan.ara@gmail.com>
2024-11-28 18:28:01 +0600

    Add Js of Playground

--- Scripts/hide_display.js
+++ Scripts/hide_display.js
@@ -11,3 +11,31 @@ function scrnChanage(hide, show) {
 }
 
 console.log(document.styleSheets[0]);
+
+function hideScrn (hideScrnID){
+    const hiddenSection = document.styleSheets[0].cssRules[hideScrnID].style;
+    hiddenSection.removeProperty("display");
+    hiddenSection.setProperty("display", "none");
+}
+
+function showScrn (showScrnID){
+    const showSection = document.styleSheets[0].cssRules[showScrnID].style;
+    showSection.removeProperty("display");
+    showSection.setProperty("display", "flex");
+}
+
+function homeToPlayground(){
+    hideScrn(3);
+    showScrn(8);
+}
+function playgroundToScore(){
+    hideScrn(8);
+    showScrn(23);
+}
+function scoreToPlayground(){
+    hideScrn(23);
+    showScrn(8);
+    gameLoop();
+}
+
+// function 
--- Scripts/play.js
+++ Scripts/play.js
@@ -1,20 +1,75 @@
 
-const letter = "abcdefghijklmnopqrstuvwx";
-const alphabets = letter.split("");
-const randomNumber = Math.round(Math.random() * 25);
-const randomAlphabet = alphabets[randomNumber].toUpperCase();
+function createRandomAlphabet() {
+    const letter = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = letter.split("");
+    const randomNumber = Math.round(Math.random() * 25);
+    const randomAlphabet = alphabets[randomNumber].toLocaleUpperCase();
+    return randomAlphabet;
+}
 
-const textArea = document.getElementById('text');
-textArea.innerText = randomAlphabet;
-
-
-function changeSelectedColor(alph) {
+function setSelectedColor(alph) {
     const typedAlph = document.getElementById(alph);
     typedAlph.style.backgroundColor = "#9900e6";
     typedAlph.style.color = "white";
 }
 
-changeSelectedColor(randomAlphabet.toLocaleLowerCase())
+function removeSelectedColor(alph) {
+    const typedAlph = document.getElementById(alph);
+    typedAlph.style.backgroundColor = "white";
+    typedAlph.style.color = "black";
+}
 
+function gameLoop() {
+    const alph = createRandomAlphabet();
+    const textArea = document.getElementById('text');
+    textArea.innerText = alph;
+
+    setSelectedColor(alph.toLowerCase());
+}
+
+function getTextElementById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementTextValue = element.innerText;
+    const value = parseInt(elementTextValue);
+    return value;
+}
+
+function setTextElementById(elementId, value) {
+    const elementBtn = document.getElementById(elementId);
+    elementBtn.innerText = value;
+}
+
+function gameOver() {
+    playgroundToScore();
+    setTextElementById('score', 0);
+    setTextElementById('life', 5);
+}
+
+
+document.addEventListener("keyup", function (event) {
+    const typedAplh = event.key;
+    const expectedText = document.getElementById('text').innerText;
+
+    if (expectedText.toLowerCase() === typedAplh) {
+        gameLoop();
+        removeSelectedColor(typedAplh);
+
+        const currentScore = getTextElementById('score');
+        const updateScore = currentScore + 1;
+        setTextElementById('score', updateScore);
+
+    }
+    else {
+        const currentLife = getTextElementById('life');
+        const updateLife = currentLife - 1;
+        setTextElementById('life', updateLife);
+
+        if (updateLife === -1) {
+            setTextElementById('finalScore', getTextElementById('score'))
+            gameOver();
+            removeSelectedColor(expectedText.toLowerCase());
+        }
+    }
+});
 
 
